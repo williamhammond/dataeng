1acfb4df4c7121928d607812ecfc3003d45f369c
WilliamHammond <william.t.hammond@gmail.com>
2018-08-02 20:42:53 -0400

    helper function to get engima data

---
+++ utils.py
@@ -0,0 +1,20 @@
+import requests
+import os
+import csv
+
+def get_enigma_data(dataset_id, api_key):
+    """
+    get_enigma_data takes a dataset id and returns the associated csv
+    """
+    
+    base_url = "https://public.enigma.com/api/"
+    headers = {'authorization': 'Bearer {}'.format(api_key)}
+    url = base_url + 'datasets/{}'.format(dataset_id)
+    r = requests.get(url, headers=headers)
+    snapshot_id = r.json()['current_snapshot']['id']
+
+    url = base_url + 'export/{}'.format(snapshot_id)
+    dat = requests.get(url, headers=headers).content.decode('utf-8')
+
+    return r
+
