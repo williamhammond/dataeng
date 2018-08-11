1dbdda7660b0fa32d1e70141569055c2a377f4f8
WilliamHammond <william.t.hammond@gmail.com>
2018-08-11 14:51:24 -0400

    error handling

--- utils.py
+++ utils.py
@@ -1,4 +1,5 @@
 import requests
+from requests.exceptions import HTTPError
 import os
 import csv
 
@@ -10,16 +11,27 @@ def get_enigma_data(dataset_id, api_key):
     base_url = "https://public.enigma.com/api/"
     headers = {'authorization': 'Bearer {}'.format(api_key)}
     url = base_url + 'datasets/{}'.format(dataset_id)
-    r = requests.get(url, headers=headers)
+    try:
+        r = requests.get(url, headers=headers)
+        r.raise_for_status()
+    except HTTPError:
+        print 'Request to url {} returned with status code {}'.format(r.url, r.status_code)
+        return 
+    
     snapshot_id = r.json()['current_snapshot']['id']
 
     url = base_url + 'export/{}'.format(snapshot_id)
-    dat = requests.get(url, headers=headers).content.decode('utf-8')
+    try:
+        dat = requests.get(url, headers=headers).content.decode('utf-8')
+        r.raise_for_status()
+    except HTTPError:
+        print 'Request to url {} returned with status code {}'.format(r.url, r.status_code)
+        return 
 
     return dat
 
 def main():
-    get_enigma_data('', os.environ["ENIGMA_API_KEY"])
+    get_enigma_data('b2cda379-c500-4ef5-aa2a-2dfae29e3aff', os.environ["ENIGMA_API_KEY"])
 
 if __name__ == "__main__":
     main()
