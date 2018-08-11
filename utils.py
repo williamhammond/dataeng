a34adadcd3f94e5d1f00107d51a6ad4720a0be41
WilliamHammond <william.t.hammond@gmail.com>
2018-08-11 16:56:48 -0400

    lint errors

--- utils.py
+++ utils.py
@@ -1,41 +1,47 @@
+"""
+This provides helper methods to pull and manipulate data from enigma.com
+"""
+import os
+import io
 import requests
 from requests.exceptions import HTTPError
-import os
-import csv
-import io
 import pandas as pd
 
 def get_enigma_data(dataset_id, api_key):
     """
     get_enigma_data takes a dataset id and returns the associated csv
     """
-    
+
     base_url = "https://public.enigma.com/api/"
     headers = {'authorization': 'Bearer {}'.format(api_key)}
     url = base_url + 'datasets/{}'.format(dataset_id)
     try:
-        r = requests.get(url, headers=headers)
-        r.raise_for_status()
+        resp = requests.get(url, headers=headers)
+        resp.raise_for_status()
     except HTTPError:
-        print 'Request to url {} returned with status code {}'.format(r.url, r.status_code)
-        return 
-    
-    snapshot_id = r.json()['current_snapshot']['id']
+        print 'Request to url {} returned with status code {}'.format(resp.url, resp.status_code)
+        return None
+
+    snapshot_id = resp.json()['current_snapshot']['id']
 
     url = base_url + 'export/{}'.format(snapshot_id)
     try:
         dat = requests.get(url, headers=headers).content.decode('utf-8')
-        r.raise_for_status()
+        resp.raise_for_status()
     except HTTPError:
-        print 'Request to url {} returned with status code {}'.format(r.url, r.status_code)
-        return 
+        print 'Request to url {} returned with status code {}'.format(resp.url, resp.status_code)
+        return None
 
     return dat
 
 def main():
-    d = get_enigma_data('f93ffd80-6679-4a76-a86e-2ab1f4007815', os.environ['ENIGMA_API_KEY'])
-    df = pd.read_csv(io.StringIO(d))
-    print df
+    """
+    request dataset of events happening in nyc
+    """
+    dat = get_enigma_data('f93ffd80-6679-4a76-a86e-2ab1f4007815', os.environ['ENIGMA_API_KEY'])
+    event_data = pd.read_csv(io.StringIO(dat))
+
+    print event_data["event_name"].value_counts()
 
 if __name__ == "__main__":
     main()
