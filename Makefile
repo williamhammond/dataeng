bac34b890116f71ade005ce698e34ae8de375205
WilliamHammond <william.t.hammond@gmail.com>
2018-08-12 15:11:30 -0400

    create graphs using make file

--- Makefile
+++ Makefile
@@ -2,3 +2,5 @@ dataset:
 	python nycevents/make_dataset.py
 lint:
 	find . -iname "*.py" | xargs pylint 
+graphs:
+	python nycevents/visualize.py
--- nycevents/visualize.py
+++ nycevents/visualize.py
@@ -1,15 +1,10 @@
 """
 visualize reads the generated dataset and maps the locations of events
 """
-import io
-import os
 import matplotlib
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
 import pandas as pd
-from nycevents.make_dataset import get_coords, get_enigma_data
-
-DATASET_ID = 'f93ffd80-6679-4a76-a86e-2ab1f4007815'
 
 def plot_events(dat):
     """
@@ -22,16 +17,13 @@ def plot_events(dat):
 
     dat.plot(kind='scatter', x='long', y='lat', color='black',
              xlim=(-74.06, -73.77), ylim=(40.61, 40.91), s=.02, alpha=.6)
-    plt.show()
+    plt.savefig('./reports/event_locations.png')
 
 def main():
     """
     request dataset of events happening in nyc
     """
-    dat = get_enigma_data(DATASET_ID, os.environ['ENIGMA_API_KEY'])
-    event_data = pd.read_csv(io.StringIO(dat))
-    dat = get_coords(event_data)
-    dat.to_csv('./events.csv')
+    dat = pd.read_csv("./data/processed/events.csv")
     plot_events(dat)
 
 if __name__ == "__main__":
